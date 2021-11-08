a9da190b34a15c6bbdb8e1c506ed85dd2c3813a5
ArthurVerrept <arthur-verrept@hotmail.co.uk>
2021-11-08 18:21:02 +0000

    setup typeorm, postgres, and migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import config from 'ormconfig'
+import { DatabaseModule } from './database/database.module'
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule, 
+    ConfigModule.forRoot({ isGlobal: true }),
+    // TypeOrmModule.forRoot(config),
+    DatabaseModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,15 @@
-export class User {
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity()
+class User {
+    @PrimaryGeneratedColumn()
     id: number
+
+    @Column()
     name: string
+
+    @Column({ nullable: true })
+    email?: string
 }
+
+export default User
