ce392861d2f5d73882f3605f6c13d09d609afa29
ArthurVerrept <Arthur-Verrept@hotmail.co.uk>
2021-12-06 17:41:20 +0000

    fix uuid add swager declaration

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Param } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
 import Product from './entities/products.entity'
 import { ProductsService } from './products.service'
 
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
     constructor(private readonly productService: ProductsService) {
