9eb2a142ec098756a6c2e3fffb54d6bfe44fa3b8
ArthurVerrept <Arthur-Verrept@hotmail.co.uk>
2021-12-14 09:44:17 +0000

    test prev workflow

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,7 +24,7 @@ import { BrandModule } from './brand/brand.module'
     BrandModule,
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
 
 export class AppModule {}
