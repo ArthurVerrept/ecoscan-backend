8f862bb19f397a32c81edaefb474a368e198bbd3
ArthurVerrept <Arthur-Verrept@hotmail.co.uk>
2021-12-14 09:35:24 +0000

    test workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,9 +19,10 @@ module.exports = {
     "@typescript-eslint/semi": ["error", "never"],
     // enforce spaces in curly braces
     "@typescript-eslint/object-curly-spacing": ["error", "always"],
+    // no trailing commas
+    '@typescript-eslint/comma-dangle': ["error", "never"],
     // warn on the use of any
     '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/comma-dangle': ["error", "never"],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off'
