2ea74ad9abdd68a884f2f9aa6af7022a4401c916
ArthurVerrept <Arthur-Verrept@hotmail.co.uk>
2021-12-30 17:40:15 +0000

    test db connection for deployment

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -8,13 +8,17 @@ import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => (process.env.DATABASE_URL, {
+      useFactory: (configService: ConfigService) => ({
+        url: process.env.DATABASE_URL,
         type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
+        ssl: {
+          rejectUnauthorized: false
+        },
+        // host: configService.get('POSTGRES_HOST'),
+        // port: configService.get('POSTGRES_PORT'),
+        // username: configService.get('POSTGRES_USER'),
+        // password: configService.get('POSTGRES_PASSWORD'),
+        // database: configService.get('POSTGRES_DB'),
         entities: [
             'dist/src/**/*.entity.js'
         ],
